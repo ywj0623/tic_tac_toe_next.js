75a37576d24c221d33e991c5bf4359795e39d657
Bûn <43789678+ywj0623@users.noreply.github.com>
2025-05-09 01:42:59 +0800

    將 SquareBtn 組件改為懶加載，並新增加載狀態顯示

--- src/app/(home)/(template)/ChessBoard.tsx
+++ src/app/(home)/(template)/ChessBoard.tsx
@@ -1,29 +1,33 @@
 "use client"
 
-import SquareBtn from "./SquareBtn"
+import { Suspense, lazy } from "react"
+
+const SquareBtn = lazy(() => import("./SquareBtn"))
 
 export default function ChessBoard() {
   const rowColumns = [0, 1, 2]
 
-  return <div className="flex flex-col flex-wrap gap-y-5">
-    {
-      rowColumns.map((row: number, rowIndex: number)=>(
-        <div className="flex flex-row gap-x-5 mx-auto" key={rowIndex}>
-          {
-            rowColumns.map((column: number, columnIndex: number)=>(
-              <div key={columnIndex} >
-                { (row * 3) + column }
-                <SquareBtn
-                row={row}
-                column={column}
-                onClick={()=>{
-                  return null
-                }} />
-              </div>
-            ))
-          }
-        </div>
-      ))
-    }
-  </div>
+  return <Suspense fallback={<div>Loading...</div>}>
+    <div className="flex flex-col flex-wrap gap-y-5">
+      {
+        rowColumns.map((row: number, rowIndex: number)=>(
+          <div className="flex flex-row gap-x-5 mx-auto" key={rowIndex}>
+            {
+              rowColumns.map((column: number, columnIndex: number)=>(
+                <div key={columnIndex} >
+                  { (row * 3) + column }
+                  <SquareBtn
+                  row={row}
+                  column={column}
+                  onClick={()=>{
+                    return null
+                  }} />
+                </div>
+              ))
+            }
+          </div>
+        ))
+      }
+    </div>
+  </Suspense>
 }
