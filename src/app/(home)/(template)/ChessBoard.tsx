437e80703729a46068c9055511d3e7bdcd988d45
Bûn <43789678+ywj0623@users.noreply.github.com>
2025-05-09 01:15:46 +0800

    為 ChessBoard 新增類型註解，並將 SquareBtn 組件整合到棋盤顯示中

--- src/app/(home)/(template)/ChessBoard.tsx
+++ src/app/(home)/(template)/ChessBoard.tsx
@@ -1,15 +1,25 @@
 "use client"
 
+import SquareBtn from "./SquareBtn"
+
 export default function ChessBoard() {
   const rowColumns = [0, 1, 2]
 
   return <div className="flex flex-col flex-wrap gap-y-5">
     {
-      rowColumns.map((row, rowIndex)=>(
+      rowColumns.map((row: number, rowIndex: number)=>(
         <div className="flex flex-row gap-x-5 mx-auto" key={rowIndex}>
           {
-            rowColumns.map((column, columnIndex)=>(
-              <div key={columnIndex} >{ (row * 3) + column }</div>
+            rowColumns.map((column: number, columnIndex: number)=>(
+              <div key={columnIndex} >
+                { (row * 3) + column }
+                <SquareBtn
+                row={row}
+                column={column}
+                onClick={()=>{
+                  return null
+                }} />
+              </div>
             ))
           }
         </div>
