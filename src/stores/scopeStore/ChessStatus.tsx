0748d756eba909ecd31eae8eefdda5eafb9ab5c9
Bûn <43789678+ywj0623@users.noreply.github.com>
2025-05-09 17:53:39 +0800

    重構 ChessStatus 與 game.type，調整 squaresScore 的類型定義以改善狀態管理

--- src/stores/scopeStore/ChessStatus.tsx
+++ src/stores/scopeStore/ChessStatus.tsx
@@ -1,14 +1,11 @@
 import { createStore } from "zustand"
-import { SquareMarker, SingleRow, Squares } from "@/types/game.type"
-
-type SquaresScoreKeys = '0,0' | '0,1' | '0,2' | '1,0' | '1,1' | '1,2' | '2,0' | '2,1' | '2,2'
-type SquaresScore = Record<SquaresScoreKeys, number>
+import { SquareMarker, SingleRow, Squares, SquaresScore } from "@/types/game.type"
 
 export type ChessStatusState = {
   stepNumber: number
   oIsNext: boolean
-  squaresScore: SquaresScore
   squares: Squares
+  squaresScore: SquaresScore
   winner: SquareMarker
 }
 
@@ -16,6 +13,7 @@ export type ChessStatusActions = {
   accumulateStepNumber: ()=> void
   toggleOIsNext: ()=> void
   updateSquares: (result: Squares)=> void
+  updateSquaresScore: (result: SquaresScore)=> void
   updateWinner: (result: SquareMarker)=> void
 }
 
@@ -24,6 +22,7 @@ export type ChessStatusStore = ChessStatusState & ChessStatusActions
 export const defaultInitChessState: ChessStatusState = {
   stepNumber: 0,
   oIsNext: true,
+  squares: Array.from({ length: 3 }, () => [null, null, null] as SingleRow),
   squaresScore: {
     '0,0': 0,
     '0,1': 0,
@@ -35,7 +34,6 @@ export const defaultInitChessState: ChessStatusState = {
     '2,1': 0,
     '2,2': 0,
   },
-  squares: Array.from({ length: 3 }, () => [null, null, null] as SingleRow),
   winner: null,
 }
 
@@ -52,6 +50,9 @@ export const createGameStatusStore = (
     })),
     updateSquares: (result: Squares)=>set((state)=>({
       squares: state.squares = result,
+    })),
+    updateSquaresScore: (result: SquaresScore)=>set((state)=>({
+      squaresScore: state.squaresScore = result,
     })),
     updateWinner: (result: SquareMarker)=>set(()=>({
       winner: result,
--- src/types/game.type.tsx
+++ src/types/game.type.tsx
@@ -2,3 +2,6 @@ export type ChessMarker = 'X' | 'O'
 export type SquareMarker = ChessMarker | null
 export type SingleRow = [SquareMarker, SquareMarker, SquareMarker]
 export type Squares = SingleRow[]
+export type SquaresScoreKeys = '0,0' | '0,1' | '0,2' | '1,0' | '1,1' | '1,2' | '2,0' | '2,1' | '2,2'
+export type SquaresScore = Record<SquaresScoreKeys, number>
+export type blockWeightingMethodsKey = 'isAroundOpponent' | 'isInDiagonal' | 'canOccupySecondBlock' | 'canStopSecondBlock' | 'canStopCompletingLine' | 'canCompleteLine'
