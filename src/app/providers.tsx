cbb72628530e2b725da3daf2fe6a14a18692a671
Bûn <43789678+ywj0623@users.noreply.github.com>
2025-05-11 04:12:42 +0800

    修正 Providers 組件的 JSX 格式，移除多餘的換行以提升代碼整潔性

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -8,5 +8,5 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         { children }
       </ChessStatusStoreProvider>
     </GameStatusStoreProvider>
-    </>
+  </>
 }
