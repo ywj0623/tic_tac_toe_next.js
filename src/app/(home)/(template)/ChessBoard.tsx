3edc99f57fd436a43efc03383038f698cc9d4ac5
Bûn <43789678+ywj0623@users.noreply.github.com>
2025-05-11 13:41:53 +0800

    重構 ChessBoard 組件，移除不必要的 AI 移動邏輯，改為使用 useGenAiMovePosition 組件以簡化代碼

--- src/app/(home)/(template)/ChessBoard.tsx
+++ src/app/(home)/(template)/ChessBoard.tsx
@@ -4,86 +4,30 @@ import { Suspense, lazy, useEffect, useCallback } from "react"
 import { useGameStatusStore } from "@/providers/GameStatus"
 import { useChessStatusStore } from "./ChessStatusScopeStore"
 import { winnerLines } from "@/configs/gameRules.config"
+import useGenAiMovePosition from "@/uses/useGenAiMovePosition"
 import useAccumulateScore from "@/uses/useAccumulateScore"
 
 import { Squares, SquareMarker, SquaresScore, blockWeightingMethodsKey } from "@/types/game.type"
 
 const SquareBtn = lazy(() => import("./SquareBtn"))
 
-interface position {
-  row: number
-  column: number
-}
-
 export default function ChessBoard() {
+  const { genAiMovePosition } = useGenAiMovePosition()
   const { getScore } = useAccumulateScore()
   const { aiChessMarker, onGameEnd } = useGameStatusStore((state) => state)
-  const { squares, stepNumber, oIsNext, squaresScore, winner, accumulateStepNumber, toggleOIsNext, updateSquaresScore, updateSquares, updateWinner } = useChessStatusStore((state) => state)
+  const { squares, stepNumber, oIsNext, winner, accumulateStepNumber, toggleOIsNext, updateSquaresScore, updateSquares, updateWinner } = useChessStatusStore((state) => state)
   const rowColumns = [0, 1, 2]
 
-  function genAiMovePosition(): position {
-    const { row, column } = stepNumber === 0 ? genRandomPosition() : pickMostWeightPosition()
-
-    return { row, column }
+  function handleAiMove(): void {
+    const { row, column } = genAiMovePosition()
+    handleUpdateSquares(row, column)
+    return
   }
 
-  function genRandomPosition(availablePositionsIndex: number[] | null = null): position {
-    const allSequence = [ ...Array(9).keys() ]
-    let randomPosition
-
-    if (availablePositionsIndex === null){
-      const randomSequence = Math.floor(Math.random() * allSequence.length)
-      randomPosition = allSequence[randomSequence]
-    }
-
-    if (availablePositionsIndex && availablePositionsIndex.length === 1) {
-      randomPosition = availablePositionsIndex[0]
-    }
-
-    if (availablePositionsIndex && availablePositionsIndex.length > 1) {
-      const randomSequence = Math.floor(Math.random() * availablePositionsIndex.length)
-      randomPosition = availablePositionsIndex[randomSequence]
-    }
-
-    if (randomPosition === undefined) {
-      throw Error('randomPosition is undefined')
-    }
-
-    const result = genRowColumn(randomPosition)
-    const { row, column } = result
-    return { row, column }
-  }
-
-  function genRowColumn(randomPosition: number): position {
-    const row = Math.floor(randomPosition / 3)
-    const column = randomPosition % 3
-
-    return { row, column }
-  }
-
-  function pickMostWeightPosition(): position {
-    const scoresList = Object.values(squaresScore)
-    const maxScore = scoresList.sort((a, b) => b - a)[0]
-    const matchedPositionList = scoresList.filter((item) => item === maxScore)
-
-    let matchedPositionIndex = scoresList.indexOf(maxScore)
-    let indices: number[] = []
-    let result: position
-
-    if (matchedPositionList.length > 1) {
-      scoresList.forEach((item, index) => {
-        if (item === maxScore) {
-          indices = [...indices, index]
-        }
-      })
-
-      result = genRandomPosition(indices)
-    } else {
-      indices = [...indices, matchedPositionIndex]
-      result = genRandomPosition(indices)
-    }
-
-    return result
+  function handleUpdateSquares(row: number, column: number): void {
+    const result: Squares = genSquares(row, column)
+    updateChessStatusStore(result)
+    return
   }
 
   function checkWinner(squares: Squares): SquareMarker {
@@ -102,18 +46,6 @@ export default function ChessBoard() {
     })
 
     return null
-  }
-
-  function handleUpdateSquares(row: number, column: number): void {
-    const result: Squares = genSquares(row, column)
-    updateChessStatusStore(result)
-    return
-  }
-
-  function handleAiMove(): void {
-    const { row, column } = genAiMovePosition()
-    handleUpdateSquares(row, column)
-    return
   }
 
   const accumulateSquaresScore = useCallback((currentSquares: Squares): SquaresScore => {
